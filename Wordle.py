5293b16a0a57ee3a4fef9a0dbbe54851abd74bb8
Chris Gillin <cgillin2@gmail.com>
2022-02-21 19:56:40 -0500

    Handle Error

--- Wordle.py
+++ Wordle.py
@@ -33,29 +33,39 @@ class App_Window:
         self.tk.bind("<F11>", self.toggle_fullscreen)
         self.tk.bind("<Escape>", self.end_fullscreen)
 
-    #----------------------------------------------------------------#
+    #----------------------------------------------------------------------------------------------------#
     #   NAME: toggle_fullscreen
     #   DESCRIPTION: Trigger to toggle fullscreen
     #   PARAMS: self - App_Window object
     #           event
     #   RETURN: "break"
-    #----------------------------------------------------------------#
+    #----------------------------------------------------------------------------------------------------#
     def toggle_fullscreen(self, event=None):
         self.state = not self.state  # Just toggling the boolean
         self.tk.attributes("-fullscreen", self.state)
         return "break"
 
-    #----------------------------------------------------------------#
+    #----------------------------------------------------------------------------------------------------#
     #   NAME: end_fullscreen
     #   DESCRIPTION: Trigger to exit fullscreen
     #   PARAMS: self - App_Window object
     #           event
     #   RETURN: "break"
-    #----------------------------------------------------------------#
+    #----------------------------------------------------------------------------------------------------#
     def end_fullscreen(self, event=None):
         self.state = False
         self.tk.attributes("-fullscreen", False)
         return "break"
+    
+    #----------------------------------------------------------------------------------------------------#
+    #   NAME: handleError
+    #   DESCRIPTION: Function to display error to user
+    #   PARAMS: self - App_Window object
+    #           e - Error string to display
+    #   RETURN: None
+    #----------------------------------------------------------------------------------------------------#
+    def handleError(self,e):
+        messagebox.showerror('Python Error', e)
 
 #----------------------------------------------------------------------------------------------------#
 #   Entry Point for initialization
